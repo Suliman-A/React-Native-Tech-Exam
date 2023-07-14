b876570068f862fdda6a5c61496a550ebcd0eba7
Abdalla Suliman <abdalla@MacOSs-MacBook-Pro.local>
2023-07-14 08:20:54 +0800

    add video player

--- Screens/UIExam.js
+++ Screens/UIExam.js
@@ -1,17 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Octicons } from '@expo/vector-icons'; 
-import AppDrawer from '../Components/AppDrawer';
+import { View, StyleSheet, Button } from 'react-native';
+import { Video, ResizeMode } from 'expo-av'
 
 const UIExam = () => {    
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>UIExam</Text>
+    <View style={styles.container}>
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+        }}
+        useNativeControls
+        resizeMode={ResizeMode.CONTAIN}
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
+        />
+      </View>
     </View>
   )
 }
 
 export default UIExam
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 20 },
+  video: { height: 300 },
+
+})
